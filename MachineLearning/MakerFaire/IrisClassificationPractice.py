e29b291eb68c0617798e2be316bb34ce8e86821c
Swimotter <mr.jacksonrubiano@gmail.com>
2022-01-11 18:26:25 -0800

    Imported libraries

---
+++ MachineLearning/MakerFaire/IrisClassificationPractice.py
@@ -0,0 +1,26 @@
+#Followed guide: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
+
+import sys
+import scipy as sp
+import numpy as np
+import matplotlib
+import pandas as pd
+import sklearn as sk
+
+#libraries
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
