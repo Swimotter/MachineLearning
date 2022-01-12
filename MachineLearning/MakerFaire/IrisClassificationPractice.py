17645ab9381b2358c8aa85616e32f37cd5c667d6
Swimotter <mr.jacksonrubiano@gmail.com>
2022-01-11 18:36:48 -0800

    Added box plots and histograms

--- MachineLearning/MakerFaire/IrisClassificationPractice.py
+++ MachineLearning/MakerFaire/IrisClassificationPractice.py
@@ -29,6 +29,17 @@ url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ["sepal-length", "sepal-width", 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names = names)
 
+# shape
+print(dataset.shape)
+# head
+print(dataset.head(20))
+# descriptions
+print(dataset.describe())
+# class distribution
+print(dataset.groupby('class').size())
+
 #data visualization
+dataset.plot(kind = "box", subplots = True, layout = (2,2), sharex = False, sharey = False)
+dataset.hist()
 scatter_matrix(dataset)
 plt.show()
