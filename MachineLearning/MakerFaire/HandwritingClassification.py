7f9bcdaaf0539e2a13c23cdbdbd59ca0fd3d276c
Swimotter <mr.jacksonrubiano@gmail.com>
2022-01-12 18:26:51 -0800

    Created model def

--- MachineLearning/MakerFaire/HandwritingClassification.py
+++ MachineLearning/MakerFaire/HandwritingClassification.py
@@ -5,6 +5,7 @@ import numpy as np
 import keras as ks
 from tensorflow.keras import models
 from tensorflow.keras import layers
+from tensorflow.keras import optimizers
 
 def load_data():
     #load emnist dataset
@@ -34,6 +35,18 @@ def prep_img(train, test):
     test_norm /= 255.0
     return train_norm, test_norm
 
+def def_model():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation = 'relu', kernel_initializer = 'he_uniform', input_shape = (28, 28, 1)))
+    model.add(layers.MaxPooling2D(2, 2))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(100, activation = 'relu', kernel_initializer = 'he_uniform'))
+    model.add(layers.Dense(62, activation = 'softmax'))
+    opt = optimizers.SGD(learning_rate = 0.01, momentum = 0.9)
+    model.compile(optimizer = opt, loss = 'categorical_crossentropy', metrics = ['accuracry'])
+    return model
+
 x_train, y_train, x_test, y_test = load_data()
 train_norm, test_norm = prep_img(x_train, x_test)
+model = def_model()
 plt.show()
